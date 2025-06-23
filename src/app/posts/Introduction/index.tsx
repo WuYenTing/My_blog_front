21e9039b238f9cec2e17cf41af49f52d8d31cdfb
Roger Wu <Roger.Wu@westpharma.com>
2025-06-23 13:32:24 +0800

    add bg-image for introduce

--- src/app/posts/Introduction/index.tsx
+++ src/app/posts/Introduction/index.tsx
@@ -1,16 +1,16 @@
-import Background from "../../components/background";
-
 const Introduction: React.FC = () => {
   return (
-    <div className="space-y-4 ">
-      <div className="bg-[url(/introduce_bg.jpg)]"></div>
-      <Background />
-      <h2 className="text-3xl font-bold tracking-tight text-gray-100 sm:text-4xl">
+    <div className="space-y-4 bg-[url('/introduce_bg.jpg')] mask-t-from-95% mask-b-from-80% mask-x-from-90% bg-no-repeat bg-center bg-cover">
+      <div className="h-72">
+      </div>
+      <h2 className="mx-10 text-3xl font-bold tracking-tight text-gray-100 sm:text-5xl mask-none">
         From the blog
       </h2>
-      <p className="text-lg leading-8 text-gray-300">
+      <p className="mx-10 text-lg leading-8 text-gray-300 sm:text-2xl mask-none">
         Learn how to grow your business with our expert advice.
       </p>
+      <div className="h-36">
+      </div>
     </div>
   );
 };
